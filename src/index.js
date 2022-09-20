0258da01806f1f0d0ddcf99bd202ca112814ca87
Danny <danielwakeley7@gmail.com>
2022-09-20 11:56:33 +0300

    This seems to be finally working

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+//import ReactDOM from "react-dom";
+//DO NOT DELETE COMENT!!!!
+//C:\Users\danie\wakeleyapp\src
+
+
+
+
+import { Button } from "./components/Button";
+
+//Create the function to be rendered here
+
+function App() {
+
+return(
+<div className="App"> <Button>Buy Now</Button>
+</div>
+
+);
+
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<App />, root);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
