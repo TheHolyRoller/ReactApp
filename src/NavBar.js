0c9f613e02e6efb753c9d425deda2e866a068b00
Danny <danielwakeley7@gmail.com>
2022-09-22 21:46:20 +0300

    Just made some quick changes following tutorial

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React, {useState}, {useEffect} from 'react';
+
+function Navbar() {
+
+    return(
+        <>
+        <nav className="navbar">
+        <div className="navbar-container">
+        <Link> to="/"</Link>
+
+        </nav>
+        </>
+            )
+}
+
+export default NavBar;
