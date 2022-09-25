347c0b127c1710cddbbbf7c7413fe06da03eb1e8
Danny <danielwakeley7@gmail.com>
2022-09-25 11:26:47 +0300

    Added APP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
-
+//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
 
 class App extends Component {
 //Put Class Constructor here
